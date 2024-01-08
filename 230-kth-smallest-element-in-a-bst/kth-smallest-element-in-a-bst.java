c2694e9698df02eeb5d4fcffd48fe0358fef6584
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-01-08 23:38:54 +0530

    Time: 2 ms (12.24%) | Memory: 44.5 MB (36.34%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -14,71 +14,26 @@
  * }
  */
 class Solution {
-    // int count=0;
-    // public int kthSmallest(TreeNode root, int k) {
-    //     return  helper(root,k).val;
-    // }
-    // public TreeNode helper(TreeNode root,int k){
-    //     if(root == null){
-    //         return null;
-    //     }
-    //     TreeNode left = helper(root.left,k);
-    //     if(left!= null){
-    //         return left;
-    //     }
-    //     count++;
-    //     if(count == k){
-    //         return root;
-    //     }
-    //     return helper(root.right,k);
-    // }
-     
-     
-     /*   public int kthSmallest(TreeNode root, int k) {
-
+    public int kthSmallest(TreeNode root, int k) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        helper(root,minHeap,k);
 
-        //remove k elements
+        helper(root,minHeap,k);
         int ans=0;
         for(int i=0;i<k;i++){
-            ans = minHeap.poll();
+             ans=minHeap.poll();
         }
         return ans;
+
     }
 
-    private void helper(TreeNode node, PriorityQueue<Integer> minHeap, int k) {
-        if (node==null){
+    private void helper(TreeNode node,PriorityQueue<Integer> minHeap,int k){
+        if(node==null){
             return;
         }
         helper(node.left,minHeap,k);
-            minHeap.add(node.val);
+
+        minHeap.add(node.val);
+
         helper(node.right,minHeap,k);
-    }
-    */
-
-        private int k=0;
-    private int ans=0;
-    public int kthSmallest(TreeNode root, int k) {
-        this.k=k;
-        helper(root);
-
-        //remove k elements
-        return ans;
-    }
-
-    private void helper(TreeNode node) {
-        if (node==null){
-            return;
-        }
-
-        helper(node.left);
-        k--;
-        if(k==0){
-            ans = node.val;
-            return;
-        }
-
-        helper(node.right);
     }
 }
