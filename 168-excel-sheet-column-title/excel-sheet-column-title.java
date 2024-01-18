10e9aa218d792667f9c7845327d0cf047799cdce
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-01-18 23:38:33 +0530

    Time: 6 ms (21.41%) | Memory: 41.3 MB (8.45%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convertToTitle(int n) {
+       String res = "";
+       while(n>0){
+           char ch = (char)('A' + (n-1)%26);
+           res = ch + res;
+           n = (n-1)/26;
+       }
+       return res;
+    }
+}
