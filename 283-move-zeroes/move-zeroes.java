aeebdccf5cf5593255dec565f9136d1e03f1215c
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-01-27 16:54:07 +0530

    Time: 2 ms (79.83%) | Memory: 46.4 MB (13.12%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,17 +1,38 @@
 class Solution {
     public void moveZeroes(int[] a) {
             int n=a.length;
-           ArrayList<Integer> list = new ArrayList<>();
-
+            int j=-1;
             for(int i=0;i<n;i++){
-                if(a[i]!=0)
-                list.add(a[i]);
+                if(a[i]==0){
+                    j=i;
+                    break;
+                }
             }
-            for(int i=0;i<list.size();i++){
-                a[i]=list.get(i);
+            if(j==-1) return ;
+            for(int i=j+1;i<n;i++){
+                if(a[i]!=0){
+                    int temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                j++;
+                }
             }
-            for(int i=list.size();i<n;i++){
-                a[i]=0;
-            }
+            
+            
+            
+            
+            // int n=a.length;
+            // ArrayList<Integer> list = new ArrayList<>();
+
+            // for(int i=0;i<n;i++){
+            //     if(a[i]!=0)
+            //     list.add(a[i]);
+            // }
+            // for(int i=0;i<list.size();i++){
+            //     a[i]=list.get(i);
+            // }
+            // for(int i=list.size();i<n;i++){
+            //     a[i]=0;
+            // }
     }
 }
