c828e4320c0569f864bdee72e0202e9c4a85fc17
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-01-29 11:22:56 +0530

    Time: 2 ms (92.83%) | Memory: 44.7 MB (45.85%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // Better Approach TC O(N) SC O(N)
         int[] res = new int[2];
         HashMap<Integer,Integer> hashmap = new HashMap<>();
         for(int i=0;i<nums.length;i++){
