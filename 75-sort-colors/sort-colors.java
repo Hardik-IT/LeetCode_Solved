b370cb0dc636e36fc2fb1d3af11ccf912822b0a6
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-01-29 12:17:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (43.99%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,22 +1,43 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int countZero=0,countOne=0,countTwo=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0)
-                countZero++;
-            else if(nums[i]==1)
-                countOne++;
-            else 
-                countTwo++;
+        // DNF TC - O(N) SC - O(1)
+        int low=0,mid=0,high=nums.length-1;
+        while(mid <= high){
+            if(nums[mid]==0){
+                int temp = nums[mid];
+                nums[mid]=nums[low];
+                nums[low]= temp;
+                mid++;
+                low++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp = nums[mid];
+                nums[mid]=nums[high];
+                nums[high]= temp;
+                high--;
+            }
         }
-        for(int i=0;i<countZero;i++){
-            nums[i] = 0;
-        }
-        for(int i=countZero;i<countZero+countOne;i++){
-            nums[i] = 1;
-        }
-        for(int i=countZero+countOne;i<nums.length;i++){
-            nums[i] = 2;
-        }
+
+
+        // Brute Force TC - O(2N) SC - O(1)
+        // int countZero=0,countOne=0,countTwo=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==0)
+        //         countZero++;
+        //     else if(nums[i]==1)
+        //         countOne++;
+        //     else 
+        //         countTwo++;
+        // }
+        // for(int i=0;i<countZero;i++){
+        //     nums[i] = 0;
+        // }
+        // for(int i=countZero;i<countZero+countOne;i++){
+        //     nums[i] = 1;
+        // }
+        // for(int i=countZero+countOne;i<nums.length;i++){
+        //     nums[i] = 2;
+        // }
     }
 }
