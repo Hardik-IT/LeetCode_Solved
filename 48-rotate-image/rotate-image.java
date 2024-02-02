fd273075866d6eab151174f75e8a58ebc8ffd4d1
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-02-02 16:03:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (24.33%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,26 +1,44 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
-        int rows=0;
-        int cols=matrix[0].length-1;
-        int[][] resMatrix=new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                resMatrix[j][cols]=matrix[i][j];
+        //Transpose
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[j][i];
+                matrix[j][i]=matrix[i][j];
+                matrix[i][j]=temp;
             }
-            rows++;
-            cols--;
         }
+        //reverse rows
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp=matrix[i][n-1-j];
+                matrix[i][n-1-j]=matrix[i][j];
+                matrix[i][j]=temp;
+            }
+        }
+        // Brute Force
+        // int n=matrix.length;
+        // int rows=0;
+        // int cols=matrix[0].length-1;
+        // int[][] resMatrix=new int[n][n];
         // for(int i=0;i<n;i++){
         //     for(int j=0;j<n;j++){
-        //         System.out.print(resMatrix[i][j]+" ");
+        //         resMatrix[j][cols]=matrix[i][j];
+        //     }
+        //     rows++;
+        //     cols--;
+        // }
+        // // for(int i=0;i<n;i++){
+        // //     for(int j=0;j<n;j++){
+        // //         System.out.print(resMatrix[i][j]+" ");
+        // //     }
+        // // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         matrix[i][j]=resMatrix[i][j];
         //     }
         // }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j]=resMatrix[i][j];
-            }
-        }
         
     }
 }
