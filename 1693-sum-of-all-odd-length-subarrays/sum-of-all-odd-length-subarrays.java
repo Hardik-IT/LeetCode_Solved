5cbea21c0e253341a045e2f3b101a46725beaf6b
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-02-03 23:05:40 +0530

    Time: 2 ms (61.53%) | Memory: 41.2 MB (49.05%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.java
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.java
@@ -2,15 +2,13 @@ class Solution {
     public int sumOddLengthSubarrays(int[] nums) {
         int oddL=0;
         for(int i=0;i<nums.length;i++){
-            int sum=0;
+            int subarraySum=0;
             for(int j=i;j<nums.length;j++){
-                if((j-i+1)%2!=0){
-                    for (int k = i; k <= j; k++) {
-                        sum += nums[k];
-                    }
-                }
+                subarraySum += nums[j];
+                if ((j - i + 1) % 2 != 0) {
+                    oddL += subarraySum;
+                }                
             }
-            oddL+=sum;
         }
         return oddL;
     }
