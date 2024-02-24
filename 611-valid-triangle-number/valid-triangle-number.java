19b96974848acb12450cfc11eb580fb5b9a42edb
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-02-24 23:08:43 +0530

    Time: 104 ms (38.52%) | Memory: 43.1 MB (67.86%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.java
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -1,4 +1,19 @@
 class Solution {
+
+    //  Time complexity - O(n^2(log(n)))
+
+    public int binarySearch(int[] nums,int l,int r,int sum){
+        while(r >= l  &&  r < nums.length){
+            int mid = (l + r)/2;
+            if(nums[mid] >= sum){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
     public int triangleNumber(int[] nums) {
 
         // Time complexity - O(n^2)
@@ -7,9 +22,7 @@ class Solution {
         for(int i=0;i<nums.length - 2; i++){
             int k = i + 2;
             for(int j = i + 1; j < nums.length - 1 && nums[i] != 0; j++){
-                while(k < nums.length && nums[i] + nums[j] > nums[k]){
-                    k++;
-                }
+                k  =  binarySearch(nums, k , nums.length - 1, nums[i] + nums[j]);
                 count += k - j - 1;
             }
         }
