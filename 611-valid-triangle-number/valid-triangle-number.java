23e6a43f40d935566c35d9ab1e13a8f10b1b847e
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-02-24 22:59:24 +0530

    Time: 33 ms (48.63%) | Memory: 43 MB (80.70%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.java
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -1,5 +1,7 @@
 class Solution {
     public int triangleNumber(int[] nums) {
+
+        // Time complexity - O(n^2)
         Arrays.sort(nums);
         int count=0;
         for(int i=0;i<nums.length - 2; i++){
