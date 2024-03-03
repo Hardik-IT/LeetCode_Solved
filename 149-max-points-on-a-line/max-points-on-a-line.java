988cc7bd6d11a8b65ad508cb7ae582967f70ef21
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-03-03 14:01:21 +0530

    Time: 26 ms (61.36%) | Memory: 44.8 MB (29.41%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.java
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -1,24 +1,39 @@
-class Solution {
-    public int maxPoints(int[][] points) {
-        if(points.length <= 2) return points.length;
-        int res=0;
-        for(int i=0;i<points.length;i++){
-            for(int j=i+1;j<points.length;j++){
-                int ans=2;
+public class Solution {
+   public int maxPoints(int[][] points) {
+        if (points.length <= 2) return points.length;
+
+        int maxCount = 0;
+        for (int i = 0; i < points.length; i++) {
+            Map<String, Integer> slopeCount = new HashMap<>();
+            int ans = 2;
+            for (int j = i + 1; j < points.length; j++) {
                 int num = points[j][1] - points[i][1];
-                int denom = points[j][0] - points[i][0];                    
-                for(int k=0;k<points.length;k++){
-                    if(k != i && k != j){
-                        int num_ = points[k][1] - points[i][1];
-                        int denom_ = points[k][0] - points[i][0];     
-                        if(num_ * denom == num * denom_){
-                            ans++;
-                        }
-                    }
-                }
-                res = Math.max(res,ans);
+                int denom = points[j][0] - points[i][0];
+                
+                String slope = getFraction(num, denom);
+                slopeCount.put(slope, slopeCount.getOrDefault(slope, 0) + 1);
             }
+            for (int count : slopeCount.values()) {
+                ans = Math.max(ans, count + 1);
+            }
+            maxCount = Math.max(maxCount, ans);
         }
-        return res;
+        return maxCount;
+    }
+    
+    private String getFraction(int num, int denom) {
+        if (denom == 0) return "inf"; // Vertical line
+        
+        int gcd = gcd(num, denom);
+        return (num / gcd) + "/" + (denom / gcd);
+    }
+    
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
