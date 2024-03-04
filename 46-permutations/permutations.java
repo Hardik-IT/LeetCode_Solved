eb3daf199bd3f37040473e91f6f81643ac00de1f
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-03-05 01:01:07 +0530

    Time: 2 ms (49.04%) | Memory: 44.3 MB (67.20%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        helper(nums,new ArrayList<>(), ans);
-        return ans;
+        List<List<Integer>> res = new ArrayList<>();
+        helper(nums,new ArrayList<>(),res);
+        return res;
     }
-    public void helper(int[] nums,List<Integer> usedElement,List<List<Integer>> ans){
-        if(nums.length==usedElement.size()){
-            ans.add(new ArrayList<>(usedElement));
+    public void helper(int[] nums,List<Integer> used,List<List<Integer>> res){
+        if(nums.length == used.size()){
+            res.add(new ArrayList<>(used));
             return;
         }
         for(int i=0;i<nums.length;i++){
-            if(!usedElement.contains(nums[i])){
-                usedElement.add(nums[i]);
-                helper(nums,usedElement,ans);
-                usedElement.removeLast();
+            if(!used.contains(nums[i])){
+                used.add(nums[i]);
+                helper(nums,used,res);
+                used.removeLast();
             }
         }
     }
