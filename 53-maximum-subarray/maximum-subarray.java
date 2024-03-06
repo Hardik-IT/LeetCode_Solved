0e67075adc30225ffb53811d68d42828ccc7f32f
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-03-06 21:54:42 +0530

    Time: 1 ms (99.57%) | Memory: 56.8 MB (82.18%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,7 +1,5 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-       
-        // Step 3 - Optimize to O(n)
         int n=nums.length;   
         int maxSum=Integer.MIN_VALUE; 
         int currSum=0;     
@@ -15,37 +13,5 @@ class Solution {
 			}
 		}
 		return maxSum;
-
-
-        // Step 2 - Brute Force - TLE Error O(n^2)    
-        // int maxSum=Integer.MIN_VALUE; 
-        // int n=nums.length;        
-        // for(int i=0;i<n;i++){
-        //      int currSum=0;
-        //     for(int j=i;j<n;j++){
-        //           currSum+=nums[j];
-        //          if(currSum>maxSum){
-        //             maxSum=currSum;
-        //         }
-        //     }           
-        // }
-        // return maxSum;
-
-        // Step 1 - Brute Force -> TLE Error O(n^3)
-        // int maxSum=Integer.MIN_VALUE; 
-        // int n=nums.length;        
-        // for(int i=0;i<n;i++){
-        //     for(int j=i;j<n;j++){
-        //          int currSum=0;
-        //          for(int k=i;k<=j;k++){
-        //             currSum+=nums[k];
-        //         }
-        //         if(currSum>maxSum){
-        //             maxSum=currSum;
-        //         }
-        //     }           
-        // }
-        // return maxSum;
-
     }
 }
