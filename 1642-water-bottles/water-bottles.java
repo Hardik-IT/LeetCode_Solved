133017e98b3c88f81b312b83ba41598652f83b02
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-03-07 23:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (33.54%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int sum = numBottles;
+        while(numBottles >= numExchange){
+            int rem = numBottles % numExchange;
+            numBottles = numBottles/numExchange;
+            sum+= numBottles;
+            numBottles += rem;
+        }
+        return sum;
+    }
+}
