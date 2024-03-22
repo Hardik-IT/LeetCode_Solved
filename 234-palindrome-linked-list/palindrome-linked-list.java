31dc7074d96a5a6f9396fe51c4edc736481e016c
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-03-22 18:12:15 +0530

    Time: 6 ms (49.88%) | Memory: 69.1 MB (10.48%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -14,43 +14,38 @@ class Solution {
         ListNode headSecond = reverseList(mid);
         ListNode reReverseHead = headSecond;
 
-
-        //Comparing both half
-        while(head!=null && headSecond !=null){
+        while(head!=null && headSecond!=null){
             if(head.val != headSecond.val){
                 break;
             }
-            head= head.next;
+            head = head.next;
             headSecond = headSecond.next;
         }
-         reverseList(reReverseHead);
-
-         return head == null || headSecond == null;
+        reverseList(reReverseHead);
+        return head == null || headSecond==null;
     }
-
     public ListNode middleNode(ListNode head) {
-        ListNode s = head;
-        ListNode f = head;
+        ListNode s=head;
+        ListNode f=head;
         while(f!=null && f.next!=null){
-            s = s.next;
-            f = f.next.next;
+            s=s.next;
+            f=f.next.next;
         }
         return s;
     }
     public ListNode reverseList(ListNode head) {
-        if(head == null){
+        if(head==null)
             return head;
+        ListNode prev=null;
+        ListNode pres=head;
+        ListNode next = pres.next;
+        while(pres!=null){
+            pres.next = prev;
+            prev = pres;
+            pres=next;
+            if(next != null)
+                next=next.next;
         }
-       ListNode prev = null;
-       ListNode pres = head;
-       ListNode next = pres.next;
-       while(pres!=null){
-           pres.next = prev;
-           prev = pres;
-           pres = next;
-           if(next != null)
-               next = next.next; 
-       }
-       return prev;
+        return prev;
     }
 }
