ebad4f34da7230048c63e30e3246a8f4771aa9a0
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-04-20 19:20:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (40.02%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -1,35 +1,31 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int matrix[][] = new int[n][n];
-        int minR=0,minC=0,maxR=n-1,maxC=n-1;
-        int count=1;
-        while(count <= n*n){
-
-            // top row
-            for(int col=minC;col <= maxC; col++){
-                matrix[minR][col]=count;
-                count++;
+        int[][] matrix = new int[n][n];
+        int minR=0,minC=0,maxC=n-1,maxR=n-1;
+        int c=1;
+        while(c <= n*n){
+            //top wall
+            for(int col=minC;col <= maxC;col++){
+                matrix[minR][col] = c;
+                c++;
             }
             minR++;
-
-            // right col
-            for(int row=minR;row<=maxR;row++){
-                matrix[row][maxC]=count;
-                count++;
+            //right wall
+            for(int row=minR;row <= maxR;row++){
+                matrix[row][maxC] = c;
+                c++;
             }
             maxC--;
-
-            // left col
-            for(int col=maxC;col>=minC;col--){
-                matrix[maxR][col]=count;
-                count++;
+            //bottom wall
+            for(int col = maxC; col>= minC;col--){
+                matrix[maxR][col] = c;
+                c++;
             }
             maxR--;
-
-            // up col
-            for(int row=maxR;row>=minR;row--){
-                matrix[row][minC]=count;
-                count++;
+            //left wall
+            for(int row=maxR; row >= minR; row--){
+                matrix[row][minC] = c;
+                c++;
             }
             minC++;
         }
