f735ddcad936ced70525ce12ad06cad6a02b8516
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-04-26 21:33:17 +0530

    Time: 1 ms (99.96%) | Memory: 45.3 MB (87.00%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
         int startI = n - 1;
-        for(int i=n - 1;i >= 0; i--){ 
+        for(int i=n - 2;i >= 0; i--){ 
             if((nums[i] + i) >= startI){
                 startI = i;
             }
