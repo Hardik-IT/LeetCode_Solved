e2858cef14ab02ea8f2b44ad41fcf622ff4ee1ea
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-04-28 21:56:13 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (99.06%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,16 +1,15 @@
 class Solution {
     public int tribonacci(int n) {
-        Map<Integer, Integer> memoMap = new HashMap<>();
-        return helper(n, memoMap);
+        if(n==0) return 0;
+        if(n == 1 || n == 2) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[n];
     }
-    private int helper(int n,Map<Integer, Integer> memo){
-        if(memo.containsKey(n))
-            return memo.get(n);
-        int res;
-        if(n == 0) res = 0;
-        else if(n == 1 || n == 2) res = 1;
-        else res = helper(n-1,memo) + helper(n-2,memo) + helper(n-3,memo);
-        memo.put(n,res);
-        return res;
-    }
+
 }
