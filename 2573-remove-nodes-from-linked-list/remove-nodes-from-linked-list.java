b420f7472d4b0e72d142fb232fbd52e7a3dcbde2
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-05-06 16:49:28 +0530

    Time: 75 ms (16.27%) | Memory: 61.7 MB (93.33%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -10,33 +10,21 @@
  */
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        head = reverse(head);
         ListNode curr = head;
-        ListNode prev = null;
-        int max = curr.val;
-        while(curr!=null){
-            max = Math.max(max,curr.val);
-            if(max > curr.val){
-                prev.next = prev.next.next;
-            }else{
-                prev = curr;
+        Stack<ListNode> stack = new Stack<>();
+        while(curr != null){
+            while(!stack.isEmpty() && stack.peek().val < curr.val){
+                stack.pop();
             }
+            stack.push(curr);
             curr = curr.next;
         }
-        return reverse(head);
-        
-    }
-    private ListNode reverse(ListNode head){
-        if(head==null) return head;
-        ListNode curr = head;
-        ListNode prev = null;
-        ListNode rLL = null;
-        while(curr!=null){
-            rLL = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = rLL;
+        ListNode nxt = null;
+        while(!stack.isEmpty()){
+            curr = stack.pop();
+            curr.next = nxt;
+            nxt = curr;
         }
-        return prev;
+        return curr;
     }
 }
