e7e50a1affcca2956d6016b8341a7c1cee21213f
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-05-20 21:53:05 +0530

    Time: 139 ms (66.17%) | Memory: 41.2 MB (81.79%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, i, j, word, 0)) {
+                if (board[i][j] == word.charAt(0) && dfs(board, i, j, word, 0)) {
                     return true;
                 }
             }
@@ -25,6 +25,5 @@ class Solution {
 
         board[i][j]=temp;
         return found;
-
     }
 }
