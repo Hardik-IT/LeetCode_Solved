ae4c9c82f95a8fc9dc958951c1a8713bddbda7ad
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-05-23 22:14:17 +0530

    Time: 6 ms (47.54%) | Memory: 45.3 MB (40.37%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int n = numCourses;
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int i=0;i<prerequisites.length;i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            graph.get(v).add(u);
+        }
+
+        int[] indegree = new int[n];
+        int[] topSort = new int[n];
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(Integer item : graph.get(i)){
+                    indegree[item]++;
+                
+            }
+        }
+        int count = 0;
+        
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 0){
+                q.add(i);
+                count++;
+            }
+        }
+        int idx = 0;
+        while(!q.isEmpty()){
+            Integer node = q.poll();
+            topSort[idx] = node;
+            idx++;
+            for(int item : graph.get(node)){
+                indegree[item]--;
+                if(indegree[item]==0){
+                    q.add(item);
+                    count++;
+                }
+            }
+        }
+        return count == numCourses;
+    }
+}
