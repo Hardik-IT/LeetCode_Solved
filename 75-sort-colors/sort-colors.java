e840e818aa910d0d18a4d6ecf0d1fd5cfa52e94c
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2024-06-12 23:01:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (26.86%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,43 +1,25 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // DNF TC - O(N) SC - O(1)
-        int low=0,mid=0,high=nums.length-1;
+        
+        int n = nums.length;
+        int low=0; // 0 - low-1 (Zeros)
+        int mid=0; // low - mid-1 (Ones)
+        int high = n-1; //mid - n-1 (unserted - 2)
         while(mid <= high){
             if(nums[mid]==0){
                 int temp = nums[mid];
-                nums[mid]=nums[low];
-                nums[low]= temp;
-                mid++;
+                nums[mid] = nums[low];
+                nums[low] = temp;
                 low++;
+                mid++;
             }else if(nums[mid]==1){
                 mid++;
             }else{
-                int temp = nums[mid];
+                int temp= nums[mid];
                 nums[mid]=nums[high];
-                nums[high]= temp;
+                nums[high]=temp;
                 high--;
             }
         }
-
-
-        // Brute Force TC - O(2N) SC - O(1)
-        // int countZero=0,countOne=0,countTwo=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]==0)
-        //         countZero++;
-        //     else if(nums[i]==1)
-        //         countOne++;
-        //     else 
-        //         countTwo++;
-        // }
-        // for(int i=0;i<countZero;i++){
-        //     nums[i] = 0;
-        // }
-        // for(int i=countZero;i<countZero+countOne;i++){
-        //     nums[i] = 1;
-        // }
-        // for(int i=countZero+countOne;i<nums.length;i++){
-        //     nums[i] = 2;
-        // }
     }
 }
