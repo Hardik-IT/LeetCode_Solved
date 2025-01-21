3cfc1df18ef8ef2f6a9a32028d950516b7d2fedc
Hardik Rathod <70482692+Hardik-IT@users.noreply.github.com>
2025-01-21 10:25:18 +0530

    Time: 4 ms (97.41%) | Memory: 57.7 MB (48.19%) - LeetSync

---
+++ 2145-grid-game/grid-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long gridGame(int[][] grid) {
+        int m = grid.length,n = grid[0].length;
+        long minResult = Long.MAX_VALUE;
+        long row1Sum = 0;
+        for (int i = 0; i < grid[0].length; i++) {
+            row1Sum += grid[0][i];
+        }
+        long row2Sum = 0;
+        for (int i = 0; i < grid[0].length; i++) {
+            row1Sum -= grid[0][i];
+            minResult = Math.min(minResult, Math.max(row1Sum, row2Sum));
+            row2Sum += grid[1][i];
+        }
+        return minResult;
+    }
+}
